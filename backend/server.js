a1bf528d8b38a99e49ff0088db04bcfd56261eb7
kabita <kabeetagiree45@gmail.com>
2026-07-15 15:52:10 +0545

    backend setup and wrote server.js code

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req,res) => {
+    res.send("it's not working")
+});
+
+app.get("/login", (req,res) => {
+    res.send("<h1>please login</h1>")
+});
+
+app.listen(port, ()=> {
+console.log(`server is running on port ${port}`)
+});
