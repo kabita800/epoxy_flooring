2f1b204725bd08bacee03159602f9761974dd613
kabita <kabeetagiree45@gmail.com>
2026-07-16 12:40:29 +0545

    setting up

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
+import express from 'express';
+// require('dotenv').config()
 const app = express();
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
     res.send("it's not working")
 });
 
-app.get("/login", (req,res) => {
-    res.send("<h1>please login</h1>")
-});
+// app.get("/login", (req,res) => {
+//     res.send("<h1>please login</h1>")
+// });
 
 app.listen(port, ()=> {
-console.log(`server is running on port ${port}`)
+console.log(`serve at http://localhost:${port}`)
 });
